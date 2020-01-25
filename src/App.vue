04e2e6a88bfd5c14a2c123de4b745aa9781ea493
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 19:11:49 +0800

    Setting Up and Loading Routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
+<script src="../../../../../AppData/Local/Temp/Rar$DRa0.615/Section Code (Finished)/src/routes.js"></script>
+<script src="../../../../../AppData/Local/Temp/Rar$DRa0.481/Section Code (Finished)/src/routes.js"></script>
 <template>
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
+                <hr>
+                <router-view></router-view>
             </div>
         </div>
     </div>
