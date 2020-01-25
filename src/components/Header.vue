31a2574bb61a6f7c2d8661dcd19b18ffbf467aeb
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 22:11:37 +0800

    create Header component and link for sending the request to server

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/user">User</a>
+        </li>
+    </ul>
+</template>
