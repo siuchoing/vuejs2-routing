3b8abbb8646bf548fbe92baddb06dc3dceefffb1
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 01:30:33 +0800

    Setting Up Child Routes (Nested Routes)

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -3,6 +3,8 @@
         <h1>The User Page</h1>
         <hr>
         <button class="btn btn-primary" @click="navigrateToHome">Go to Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
 
