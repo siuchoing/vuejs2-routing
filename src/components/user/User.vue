75f9a35a179bd094abcb9a70eef4b202a8604aba
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 23:50:30 +0800

    Fetching and using route parameters by $route object provided by vuejs

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,19 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="navigrateToHome">Go to Home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                // $route is route object provided by vuejs
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigrateToHome() {
                 this.$router.push('/');
