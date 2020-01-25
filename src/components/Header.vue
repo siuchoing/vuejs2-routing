75eacaf5b8f06f8774aff3c4842a26d7b66bffd7
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 22:48:25 +0800

    Styling Active Links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
+<!----------------------------------------------------------------------
+|  to="/"  // only loading routes, without sending request to server
+|  exact   // only match this url '/', rather than match the url '/'.'user' as well
+------------------------------------------------------------------------>
 <template>
     <ul class="nav nav-pills">
-        <li class="nav-item">
-            <!--without sending request to server-->
-            <router-link to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link to="/user">User</router-link>
-        </li>
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
     </ul>
 </template>
