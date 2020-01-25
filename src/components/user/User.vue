e14c1bbe9f3fb069bd18e6cf42ff5dc8b4f97992
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 23:01:52 +0800

    edit html code

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,13 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary">Go to Home</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        
+    }
+</script>
