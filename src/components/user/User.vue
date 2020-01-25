ce04c3b1dc609004f7514c9d4b66a5515a3e0fd1
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 23:07:18 +0800

    Navigating to the router path (Imperative Navigation)

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,16 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <button class="btn btn-primary">Go to Home</button>
+        <button class="btn btn-primary" @click="navigrateToHome">Go to Home</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            navigrateToHome() {
+                this.$router.push('/');
+            }
+        }
     }
 </script>
