f1e23e7a105383c5c7577b10f63d0d53f036ee7c
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 20:10:31 +0800

    set up beforeRouteLeave guard to confirm leaving that page

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -3,7 +3,31 @@
         <h3>Edit the User</h3>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <hr>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm to leave</button>
         <div style="height: 700px"></div>
         <p id="data">Some extra Data</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                confirmed : false
+            }
+        },
+        beforeRouteLeave (to, from, next) {
+            if(this.confirmed) {
+                next();                 // Confirm to leave
+            } else {
+                if( confirm('Are you sure') ) {
+                    next();             // Confirm to leave
+                } else {
+                    console.log('Cancel to leave');
+                }
+            }
+
+        }
+    }
+</script>
