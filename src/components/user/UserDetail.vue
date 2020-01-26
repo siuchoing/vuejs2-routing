89f07fb679e87cd1c636d1d085a521c4f15181dd
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 15:04:54 +0800

    send query parameters from UserDetail to UserEdit component

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,10 +2,10 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
-        <!-- Named Routes from routes.js -->
+        <!-- Passing query parameters with Named Routes from routes.js -->
         <router-link
                 tag="button"
-                :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
                 class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
