955dc6063d8c70aa990c5b0baf75f077e7d9fb23
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 20:29:48 +0800

    Lazy loading part of components / applications when we need it by syntax webpack recognizers

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,32 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+/***********
+ * Eager loading all components / applications when we need it
+ */
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
 
+/***********
+ * Lazy loading part of components / applications when we need it by syntax webpack recognizers
+ */
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+};
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+};
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+};
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+};
 
 /****************************************
  * beforeEnter       // can store in another file and simply import.
