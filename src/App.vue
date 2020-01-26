7d1dd11803692079b9442b44d2e504abfb56266a
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 15:39:54 +0800

    Multiple Router Views (Named Router Views) to display diff. content

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,18 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
-                <app-header></app-header>
-                <router-view></router-view>
+                <!-- Display header in the top of Home Page -->
+                <router-view name="header-top"></router-view>
+                <!-- Display specific content from related component by different url -->
+                <router-view></router-view><br>
+                <!-- Display header in the bottom of User Page -->
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-
-    export default {
-        components: {
-            appHeader: Header
-        }
-    }
 </script>
 
 <style>
