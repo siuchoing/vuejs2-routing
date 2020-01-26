a404d983b3551b42d049851b3ec9ce256d1dcf9b
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 16:41:10 +0800

    prevent data jump at the bottom while navagate to edit page with '#data', where "http://localhost:8080/user/3/edit?locale=en&q=100#data"

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -5,7 +5,28 @@
         <!-- Passing query parameters with Named Routes from routes.js -->
         <router-link
                 tag="button"
-                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
+                :to="link"
                 class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100
+                    },
+                    // prevent data jump at the bottom while navigating to edit page with '#data', where "http://localhost:8080/user/3/edit?locale=en&q=100#data"
+                    hash: '#data'
+                }
+            }
+        }
+    }
+</script>
