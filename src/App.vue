211d50ea8eafb9bb5b8d1b057b04030e7014b0d8
altantic <15200582@life.hlkbu.edu.hk>
2020-01-26 16:21:05 +0800

    Create slide animation

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@
                 <!-- Display header in the top of Home Page -->
                 <router-view name="header-top"></router-view>
                 <!-- Display specific content from related component by different url -->
-                <router-view></router-view><br>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+                <br>
                 <!-- Display header in the bottom of User Page -->
                 <router-view name="header-bottom"></router-view>
             </div>
@@ -19,4 +22,36 @@
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
